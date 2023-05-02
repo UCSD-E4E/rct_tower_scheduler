8cabbc9e247dad149c5fedf12ecbf1be4b0c6ff9
Dylan Palmer <dcpalmer@ucsd.edu>
2023-05-01 22:06:48 -0700

    Rewrite setup to a standalone python script Also slight correction to dummy_ensembles file

---
+++ convertToActive.py
@@ -0,0 +1,73 @@
+#
+# This is a reimplementation of the cpp "setup" function as a standalone
+# script so that whoever writes/generates the ensemble list can convert it
+# into a format more easily used by the scheduler
+
+# The default read-in file is "ensembles.json and can be overwritten by
+# passing in a filename as an argument
+
+# This script always writes to "active_ensembles.json" and WILL overwrite
+# if a file of that name is in the same directory
+
+import sys
+import json
+
+
+def hmsToSeconds(hour, minute, sec):
+    return sec + minute * 60 + hour * 3600
+
+def sortFunc(ens):
+    return ens["start_time"]
+
+
+# execution starts here :)
+filein = "ensembles.json"
+if len(sys.argv) > 1 :
+    filein = sys.argv[1]
+
+f_in = open(filein)
+
+ens = json.load(f_in)
+
+ens_list = []
+json_file = []
+# double loop adds all ensembles and their iterations to a list
+for i in ens["ensemble_list"]:
+    i_hour   = i["start_time"]["hour"]
+    i_minute = i["start_time"]["minute"]
+    i_second = i["start_time"]["second"]
+    tot_time = hmsToSeconds(i_hour, i_minute, i_second)
+
+    for j in range(i["iterations"]):
+        iter_seconds = i["interval"]
+        i_obj = { "title": i["title"],
+                 "function": i["function"],
+                 "inputs": i["inputs"],
+                 "start_time": tot_time + iter_seconds * j }
+        ens_list.append(i_obj)
+
+
+# create an extra object for teardown function
+teardown_obj = { "title": "teardown",
+                "function": "teardown",
+                "inputs": [],
+                "start_time": 86399 }
+
+ens_list.append(teardown_obj)
+
+# sort all the enumerated ensembles by time
+ens_list.sort(key=sortFunc)
+
+# data that will become our json file format
+json_file = { "ensemble_list": [],
+             "next_ensemble": 0 }
+for i in ens_list:
+    json_file["ensemble_list"].append(i)
+
+# open/create file in overwrite mode
+f_out = open("active_ensembles.json", "w")
+
+# write to our file with formatted json
+f_out.write(json.dumps(json_file, indent=4))
+
+f_out.close()
