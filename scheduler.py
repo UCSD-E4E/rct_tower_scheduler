1ad4ec1a21cca617092a44a2a220b52236777a51
Wesleylewis05 <wjl4jj@virginia.edu>
2023-05-09 21:59:21 -0400

    updated scheduler.py to add ensemble function and deleted ensemble.py

--- scheduler.py
+++ scheduler.py
@@ -77,47 +77,36 @@ def setup(filename: str = "dummy_ensembles.json"):
 
 def perform_ensemble_functions(filename: str = "dummy_ensembles.json"):
     '''
-    use importlib
     Function to call all dummy_ensembles.json functions for the day
     @param filename: specifies file with ensemble specifications
     '''
-    
-    with open(filename) as user_file:
+    with open(filename, encoding="utf-8") as user_file:
         file_contents = json.load(user_file)
-
-    
     for i in file_contents['ensemble_list']:
-
         module_name = i["module_name"]
         module_directory = i["module_directory"]
         class_name = i["class"]
         function_name = i["function"]
         function_inputs = i["inputs"]
-
         # Load module
         # try:
         spec = importlib.util.spec_from_file_location(module_name,module_directory)
         module = importlib.util.module_from_spec(spec)
         # except ModuleNotFoundError:
-        #     raise ModuleNotFoundError("JSON module_directory: {} is not found".format(module_directory))
+        # raise ModuleNotFoundError("JSON module_directory: \
+        # {} is not found".format(module_directory))
         spec.loader.exec_module(module)
-
         # If there is no class
         if not class_name:
             # Get function from module
             class_function = getattr(module, function_name)
-            
             # Run function
             class_function(*function_inputs)
-
         else: # We're accessing a function from a class
-
             # Get class from module
             module_class = getattr(module, class_name)
-
             # Get function from class
             class_function = getattr(module_class, function_name)
-            
             # Run function
             class_function(*function_inputs)
 
@@ -149,7 +138,7 @@ def main():
 
         ensembles["next_ensemble"] += 1
         ensemble_ofile = open(filename, "w")
-        json.dump(full_json, ensemble_ofile)
+        #json.dump(full_json, ensemble_ofile)
         ensemble_ofile.close()
 
         ''' TODO: Matthew
@@ -179,18 +168,10 @@ def main():
     print("now in seconds: " + str(curr_time_seconds))
     print("next ensemble time in seconds: " + str(nearest_ens_time))
 
-    '''
-    TODO: Wesley -- Done
-
-    ensemble = active_ensembles["ensemble_list"][next_ensemble]
-
-    # perform ensemble functions
-    for (auto f : ensemble)
-    	std::invoke(f, inputs) # Wesley
-    	# TODO: allow for function arguments via e["inputs"]
-		# should just be comma-separated list of inputs
+    print("performing ensemble functions: ")
+    perform_ensemble_functions()
     next_ensemble += 1
-    '''
+    
 
     '''
      TODO: Matthew
