a2da0504e182655d9637b1c505e850d4c5552c44
Wesleylewis05 <wjl4jj@virginia.edu>
2023-05-09 21:22:36 -0400

    Added function to run ensemble functions from json file

---
+++ ensemble_tests/tester_functions.py
@@ -0,0 +1,18 @@
+class SimpleFunctions():
+    def hello_world():
+        print("Hello World")
+
+    def print_this(string):
+        print(string)
+
+    def add_this(x, y):
+        print(x + y)
+
+    def subtract_this(x, y):
+        print(x - y)
+
+    def multiply_this(x, y):
+        print(x * y)
+
+def no_class():
+    print("There is no class for this function")
--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import time
+import importlib.util
 
 TIME_SHUTDOWN = 5 # find real shutdon and wakeup times later
 TIME_WAKEUP = 5
@@ -74,6 +75,52 @@ def setup(filename: str = "dummy_ensembles.json"):
     ensemble_ofile.close()
 
 
+def perform_ensemble_functions(filename: str = "dummy_ensembles.json"):
+    '''
+    use importlib
+    Function to call all dummy_ensembles.json functions for the day
+    @param filename: specifies file with ensemble specifications
+    '''
+    
+    with open(filename) as user_file:
+        file_contents = json.load(user_file)
+
+    
+    for i in file_contents['ensemble_list']:
+
+        module_name = i["module_name"]
+        module_directory = i["module_directory"]
+        class_name = i["class"]
+        function_name = i["function"]
+        function_inputs = i["inputs"]
+
+        # Load module
+        # try:
+        spec = importlib.util.spec_from_file_location(module_name,module_directory)
+        module = importlib.util.module_from_spec(spec)
+        # except ModuleNotFoundError:
+        #     raise ModuleNotFoundError("JSON module_directory: {} is not found".format(module_directory))
+        spec.loader.exec_module(module)
+
+        # If there is no class
+        if not class_name:
+            # Get function from module
+            class_function = getattr(module, function_name)
+            
+            # Run function
+            class_function(*function_inputs)
+
+        else: # We're accessing a function from a class
+
+            # Get class from module
+            module_class = getattr(module, class_name)
+
+            # Get function from class
+            class_function = getattr(module_class, function_name)
+            
+            # Run function
+            class_function(*function_inputs)
+
 def someFunc():
     ''''
     simple printing function for use in testing
@@ -133,7 +180,7 @@ def main():
     print("next ensemble time in seconds: " + str(nearest_ens_time))
 
     '''
-    TODO: Wesley
+    TODO: Wesley -- Done
 
     ensemble = active_ensembles["ensemble_list"][next_ensemble]
 
