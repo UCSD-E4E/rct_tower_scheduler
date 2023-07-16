b2cd24529a7e2c4e4890f717b02638f9073c9332
Matthew Peng <mapeng@ucsd.edu>
2023-07-16 16:27:48 -0700

    first try at testing scheduler

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,1 +1,8 @@
-# TODO
+from TowerScheduler.scheduler import *
+
+def test_wakeup_transition():
+    state_machine = StateMachine()
+    wake_up = WakeUp()
+    wake_up.update(state_machine)
+
+    assert state_machine.curr_state == state_machine.check_time_state
