5761e266f8b6339840609043576d1978b7a12f0b
hgrehm <hgrehm@ucsd.edu>
2023-07-27 10:31:09 -0700

    fixture usage and using update function's returns

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,28 +1,24 @@
 import pytest
 import time
-from TowerScheduler.scheduler import StateMachine, \
-                    WakeUp, CheckTimePath, CheckTime, \
-                    Sleep, PerformEnsemble, Iterate
+from TowerScheduler.ensemble import Ensemble
+from TowerScheduler.scheduler import  CheckTimePath, StateMachine, \
+                WakeUp, CheckTime, Iterate, PerformEnsemble, Sleep
 
-
+@pytest.fixture(name='state_machine')
 def setup_test():
-    state_machine = StateMachine("../TowerScheduler/active_ensembles.json",\
-                                  time.sleep)
-    return state_machine
+    ens_list = Ensemble.list_from_json("active_ensembles.json")
+    return StateMachine(ens_list, time.sleep)
 
-
-def test_wakeup_transition():
-    state_machine = setup_test()
+def test_wakeup_transition(state_machine):
     state_machine.curr_state = WakeUp.get_singleton()
-    state_machine.curr_state.update(state_machine)
-    assert isinstance(state_machine.curr_state, CheckTime)
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, CheckTime)
 
-def test_check_time_to_perform_ensemble_transition():
-    state_machine = setup_test()
+def test_check_time_to_perform_ensemble_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
     state_machine.curr_state.check_time_ctrl = CheckTimePath.RUN
-    state_machine.curr_state.update(state_machine)
-    assert isinstance(state_machine.curr_state, PerformEnsemble)
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, PerformEnsemble)
 
 # def test_perform_ensemble(): TODO: Perform ensemble function - Wesley
 #     state_machine = StateMachine()
@@ -35,28 +31,25 @@ def test_check_time_to_perform_ensemble_transition():
 #     perform.perform_ensemble_functions()
 #     assert state_machine.curr_state == state_machine.perform_ensemble_state
 
-def test_check_time_to_sleep_transition():
-    state_machine = setup_test()
+def test_check_time_to_sleep_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
     state_machine.curr_state.check_time_ctrl = CheckTimePath.WAIT
-    state_machine.curr_state.update(state_machine)
-    assert isinstance(state_machine.curr_state, Sleep)
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, Sleep)
 
 # TODO: Fix AttributionError - Wesley
-def test_check_time_to_iterate_transition(): 
-    state_machine = setup_test()
+def test_check_time_to_iterate_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
     state_machine.curr_state.check_time_ctrl = CheckTimePath.SKIP
-    state_machine.curr_state.update(state_machine)
-    assert isinstance(state_machine.curr_state, Iterate)
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, Iterate)
 
 
-def test_check_time_reset_transition():
-    state_machine = setup_test()
+def test_check_time_reset_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
     state_machine.curr_state.check_time_ctrl = CheckTimePath.RESET
-    state_machine.curr_state.update(state_machine)
-    assert isinstance(state_machine.curr_state, CheckTime)
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, CheckTime)
 
 # TODO: Fix Infinite Loop - Matthew
 # def test_sleep_to_check_time_transition():
