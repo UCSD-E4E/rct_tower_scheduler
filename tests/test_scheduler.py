84f82f8aebfd43d1565dd3e4f54f6b2f2c20946a
Wesleylewis05 <wjl4jj@virginia.edu>
2023-08-21 19:22:44 -0400

    updated test_scheduler, removed redundant comments

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -14,7 +14,6 @@ def setup_test():
     ens_list = Ensemble.list_from_json("active_ensembles.json")
     return StateMachine(ens_list, time.sleep)
 
-
 def test_wakeup_transition(state_machine):
     state_machine.curr_state = WakeUp.get_singleton()
     new_state = state_machine.curr_state.update(state_machine)
@@ -25,17 +24,6 @@ def test_check_time_to_perform_ensemble_transition(state_machine):
     state_machine.curr_state.check_time_ctrl = CheckTimePath.RUN
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, PerformEnsemble)
-
-# def test_perform_ensemble(): TODO: Perform ensemble function - Wesley
-#     state_machine = StateMachine()
-#     wake_up = WakeUp()
-#     wake_up.update(state_machine)
-#     check_time = CheckTime()
-#     check_time.check_time_ctrl = CheckTimePath.RUN
-#     check_time.update(state_machine)
-#     perform = PerformEnsemble()
-#     perform.perform_ensemble_functions()
-#     assert state_machine.curr_state == state_machine.perform_ensemble_state
 
 def test_check_time_to_sleep_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
@@ -57,27 +45,4 @@ def test_check_time_reset_transition(state_machine):
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, CheckTime)
 
-# TODO: Fix Infinite Loop - Matthew
-# def test_sleep_to_check_time_transition():
-#     state_machine = setup_test()
-#     state_machine.curr_state = Sleep.get_singleton()
-#     state_machine.curr_state.update(state_machine)
-#     assert isinstance(state_machine.curr_state, CheckTime)
 
-# # def test_fail():
-# #     assert False
-
-# def hello_world():
-#     print("Hello World")
-
-# def print_this():
-#     print("string")
-
-# def add_this():
-#     print("x + y")
-
-# def subtract_this():
-#     print("x - y")
-
-# def multiply_this():
-#     print("x * y")
