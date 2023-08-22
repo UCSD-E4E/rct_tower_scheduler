dded3c62f8da1853f637f3730e89f350376e283d
Wesley Lewis <wjl4jj@virginia.edu>
2023-08-21 20:50:26 -0400

    added perfom_ensemble to check_time, sleep_to_check_time, and iterate_to_check_time tests

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -14,7 +14,7 @@ def setup_test():
     ens_list = Ensemble.list_from_json("active_ensembles.json")
     return StateMachine(ens_list, time.sleep)
 
-def test_wakeup_transition(state_machine):
+def test_wakeup_to_check_time_transition(state_machine):
     state_machine.curr_state = WakeUp.get_singleton()
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, CheckTime)
@@ -31,13 +31,11 @@ def test_check_time_to_sleep_transition(state_machine):
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, Sleep)
 
-# TODO: Fix AttributionError - Wesley
 def test_check_time_to_iterate_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
     state_machine.curr_state.check_time_ctrl = CheckTimePath.SKIP
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, Iterate)
-
 
 def test_check_time_reset_transition(state_machine):
     state_machine.curr_state = CheckTime.get_singleton()
@@ -45,4 +43,17 @@ def test_check_time_reset_transition(state_machine):
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, CheckTime)
 
+def test_iterate_to_check_time_transition(state_machine):
+    state_machine.curr_state = Iterate.get_singleton()
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, CheckTime)
 
+def test_sleep_to_check_time_transition(state_machine):
+    state_machine.curr_state = Sleep.get_singleton()
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, CheckTime)
+
+def test_perform_ensemble_to_check_time_transition(state_machine):
+    state_machine.curr_state = PerformEnsemble.get_singleton()
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, CheckTime)
