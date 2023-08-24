0920c9c8de13cb15a78a28533adaa800856e78ee
Wesley Lewis <wjl4jj@virginia.edu>
2023-08-24 01:29:50 -0400

    fixed sleep to wakeup transition and perform ensemble to iterate transition

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,8 +1,8 @@
 import pytest
 import time
 from pathlib import Path
-
-from TowerScheduler.config import Configuration
+import datetime as dt
+from TowerScheduler.config import Configuration, get_instance
 from TowerScheduler.ensemble import Ensemble
 from TowerScheduler.scheduler import  CheckTimePath, StateMachine, \
                 WakeUp, CheckTime, Iterate, PerformEnsemble, Sleep
@@ -48,12 +48,16 @@ def test_iterate_to_check_time_transition(state_machine):
     new_state = state_machine.curr_state.update(state_machine)
     assert isinstance(new_state, CheckTime)
 
-# def test_sleep_to_check_time_transition(state_machine):
-#     state_machine.curr_state = Sleep.get_singleton()
-#     new_state = state_machine.curr_state.update(state_machine)
-#     assert isinstance(new_state, CheckTime)
+def test_sleep_to_wakeup_transition(state_machine):
+    state_machine.curr_state = Sleep.get_singleton()
+    config = get_instance(Configuration.default_path)
+    # Calling Python's time.sleep for 0 seconds
+    state_machine.curr_state.nearest_ens_time = dt.datetime.now()\
+         + dt.timedelta(seconds=config.execute_buffer + 1)
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, WakeUp)
 
-# def test_perform_ensemble_to_check_time_transition(state_machine):
-#     state_machine.curr_state = PerformEnsemble.get_singleton()
-#     new_state = state_machine.curr_state.update(state_machine)
-#     assert isinstance(new_state, CheckTime)
+def test_perform_ensemble_to_iterate_transition(state_machine):
+    state_machine.curr_state = PerformEnsemble.get_singleton()
+    new_state = state_machine.curr_state.update(state_machine)
+    assert isinstance(new_state, Iterate)
